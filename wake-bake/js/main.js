c31cd3f76431e9e45afe2f0eb47bfabc3ac3f567
Yura Kholodov <yurdast@mail.ru>
2024-06-25 10:59:13 +0300

    The header is adapted

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -1,6 +1,15 @@
-const burgerIcon = document.querySelector(".burger-icon");
-const body = document.querySelector("body");
+(function () {
+  document.addEventListener("click", burgerInit);
 
-burgerIcon.addEventListener("click", () => {
-  body.classList.toggle("body--opened-menu");
-});
+  function burgerInit(event) {
+    const target = event.target;
+
+    const burgerIcon = target.closest(".burger-icon");
+    const burgerNavLink = target.closest(".nav__link");
+    const clientWidth = document.documentElement.clientWidth;
+
+    if ((!burgerIcon && !burgerNavLink) || clientWidth > 900) return;
+
+    document.body.classList.toggle("body--opened-menu");
+  }
+})();
