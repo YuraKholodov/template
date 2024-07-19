449c7fecc5398a1155e6fd32d7a9df347dfe9108
Yura Kholodov <yurdast@mail.ru>
2024-07-19 17:54:52 +0300

    19.07

--- сandles/js/main.js
+++ сandles/js/main.js
@@ -5,4 +5,28 @@
   menuBtn.addEventListener("click", () => {
     menuMobile.classList.toggle("menu--open");
   });
+
+  // Слайдер feedback
+
+  new Swiper(".feedback__slider", {
+    slidesPerView: "auto",
+    autoplay: {
+      delay: 5000,
+    },
+
+    pagination: {
+      el: ".feedback__pagination",
+    },
+  });
+
+  // Слайдер certificates
+
+  new Swiper(".certificates__slider", {
+    slidesPerView: 3,
+    spaceBetween: 15,
+
+    pagination: {
+      el: ".certificates__pagination",
+    },
+  });
 })();
