b95a49baa51b2a0e058425ed574bcc9a248b8785
Yura Kholodov <yurdast@mail.ru>
2024-07-25 16:33:04 +0300

    header done

--- travel/js/main.js
+++ travel/js/main.js
@@ -1,17 +1,23 @@
 (function () {
   const body = document.querySelector("body");
   const heroForm = body.querySelector(".hero__form");
+  const modalHeroForm = body.querySelector('.modal .hero__form')
 
   heroForm.addEventListener("click", toggleDate);
+  modalHeroForm.addEventListener("click", toggleDate);
 
   function toggleDate(event) {
     const target = event.target;
     const btnDate = target.closest(".select__date-btn");
     if (!btnDate) return;
+    console.log('rl')
 
     const date = target.closest(".select__wrapper");
     const dateInput = date.querySelector(".select__date-input");
     const dateInputMobile = heroForm.querySelector(
+      ".select__date-input--mobile"
+    );
+    const modalDateInputMobile = modalHeroForm.querySelector(
       ".select__date-input--mobile"
     );
 
@@ -24,6 +30,10 @@
         dateInputMobile.style.maxHeight = 0;
         dateInputMobile.style.marginBottom = 0;
       }
+      if (modalDateInputMobile) {
+        modalDateInputMobile.style.maxHeight = 0;
+        modalDateInputMobile.style.marginBottom = 0;
+      }
     } else {
       date.classList.add("select__wrapper--active");
       if (dateInput) {
@@ -32,6 +42,10 @@
       if (dateInputMobile) {
         dateInputMobile.style.maxHeight = dateInputMobile.scrollHeight + "px";
         dateInputMobile.style.marginBottom = 10 + "px";
+      }
+      if (modalDateInputMobile) {
+        modalDateInputMobile.style.maxHeight = modalDateInputMobile.scrollHeight + "px";
+        modalDateInputMobile.style.marginBottom = 10 + "px";
       }
     }
   }
